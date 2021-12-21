983e6fd84ccc74fb30502ea0fc86228df2f0fd32
DanielAugusto191 <daniel.augusto191@gmail.com>
2021-12-21 08:24:52 -0300

    new book

---
+++ RustProgrammingLanguage/Chapter 1/hello_cargo/target/debug/deps/hello_cargo-5b1ebf27963c30fe.d
@@ -0,0 +1,5 @@
+/home/pc2/Learning-Rust/Chapter 1/hello_cargo/target/debug/deps/hello_cargo-5b1ebf27963c30fe: src/main.rs
+
+/home/pc2/Learning-Rust/Chapter 1/hello_cargo/target/debug/deps/hello_cargo-5b1ebf27963c30fe.d: src/main.rs
+
+src/main.rs:
---
+++ RustProgrammingLanguage/Chapter 1/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/pc2/Learning-Rust/Chapter\ 1/hello_cargo/target/debug/hello_cargo: /home/pc2/Learning-Rust/Chapter\ 1/hello_cargo/src/main.rs
